a4f3240c468bb3581df7078b824552cd01625f9a
syhue <sook.yan.hsy@gmail.com>
2023-07-01 13:05:23 +0800

    setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', loadChildren: () => import('./portfolio/portfolio.module').then(m => m.PortfolioModule) },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
